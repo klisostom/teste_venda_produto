1d9d96ab7c7081b4b8645f7dc4234323848515c2
Klisóstom <klis@pm.me>
2020-10-07 18:06:55 -0300

    #feature/Migrations
    Migrations criadas

--- database/migrations/2020_10_07_203001_create_tipo_produtos_table.php
+++ database/migrations/2020_10_07_203001_create_tipo_produtos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProdutosTable extends Migration
+class CreateTipoProdutosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateProdutosTable extends Migration
      */
     public function up()
     {
-        Schema::create('produtos', function (Blueprint $table) {
+        Schema::create('tipo_produtos', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
-            $table->decimal('preco', 8, 2);
+            $table->string('descricao', 100);
             $table->timestamps(0);
             $table->softDeletesTz(0);
         });
@@ -29,6 +28,6 @@ class CreateProdutosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('produtos');
+        Schema::dropIfExists('tipo_produtos');
     }
 }
