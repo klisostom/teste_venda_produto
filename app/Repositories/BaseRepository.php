09a56dc5c2c170f5b9f4b5431219d8980bfc9982
Klisóstom <klis@pm.me>
2020-10-08 09:48:12 -0300

    Criação dos repositories

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
+
+class BaseRepository implements BaseRepositoryInterface
+{
+    // model property on class instances
+    protected $model;
+
+    // Constructor to bind model to repo
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    // Get all instances of model
+    public function all()
+    {
+        return $this->model->paginate();
+    }
+
+    // create a new record in the database
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    // update record in the database
+    public function update(array $data, int $id)
+    {
+        $record = $this->model->find($id);
+        return $record->update($data);
+    }
+
+    // remove record from the database
+    public function delete(int $id)
+    {
+        return $this->model->destroy($id);
+    }
+
+    // show the record with the given id
+    public function show(int $id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    // Get the associated model
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    // Set the associated model
+    public function setModel(Model $model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    // Eager load database relationships
+    public function with($relations)
+    {
+        return $this->model->with($relations);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,14 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\ProdutoController;
+use App\Http\Controllers\API\Produto\ProdutoController;
 
 Route::group(['namespace' => 'API'], function () {
-        Route::get('/produtos/{id}', 'ProdutoController@show');
-        Route::get('/produtos', 'ProdutoController@index');
-        Route::post('/produtos', 'ProdutoController@store');
-        Route::put('/produtos/{id}', 'ProdutoController@update');
-        Route::delete('/produtos/{id}', 'ProdutoController@destroy');
-
-        Route::get('/teste', [ProdutoController::class, 'teste']);
+    Route::group(['namespace' => 'Produto'], function () {
+        Route::get('/produtos/{id}', [ProdutoController::class, 'show']);
+        Route::get('/produtos', [ProdutoController::class, 'index']);
+        Route::post('/produtos', [ProdutoController::class, 'store']);
+        Route::put('/produtos/{id}', [ProdutoController::class, 'update']);
+        Route::delete('/produtos/{id}', [ProdutoController::class, 'destroy']);
+    });
 });
